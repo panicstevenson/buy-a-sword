05a5ba16577ef458169b3e2df5631f35b40ae1b8
Panic Stevenson <Panic.Stevenson@microsoft.com>
2020-06-29 21:11:52 -0700

    Pushing from Mac

--- Player.gd
+++ Player.gd
@@ -1,7 +1,5 @@
 extends KinematicBody2D
 
-signal collided
-
 export var speed = 400  # How fast the player will move (pixels/sec).
 export var jump = -700
 export var gravity = 2500
@@ -27,20 +25,17 @@ func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
-	for i in get_slide_count():
-		var collision = get_slide_collision(i)
-		if collision:
-			emit_signal('collided', collision)
 
 	if Input.is_action_just_pressed('ui_select'):
 		if is_on_floor():
 			velocity.y = jump
 
 	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.play()
-		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment
-		$AnimatedSprite.flip_h = velocity.x < 0
+		$SpriteFlip/AnimatedSprite.animation = "walk"
+		$SpriteFlip/AnimatedSprite.play()
+		if velocity.x < 0:
+			$SpriteFlip.scale.x = -1
+		else:
+			$SpriteFlip.scale.x = 1
 	else:
-		$AnimatedSprite.stop()
+		$SpriteFlip/AnimatedSprite.stop()
