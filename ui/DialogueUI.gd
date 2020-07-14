a689f61897994b3a415df9480547c7af41da405c
ChrisLHall <mrchrislhall@gmail.com>
2020-07-13 21:06:27 -0700

    Getting dialogue UI appearing and disappearing

--- ui/DialogueUI.gd
+++ ui/DialogueUI.gd
@@ -7,12 +7,45 @@ extends Control
 
 var floatLetters = 0.0
 
+export(NodePath) var labelPath = null
+var label = null
+
+export(NodePath) var rectPath = null
+var rect = null
+
+var currentText = ""
+var showing = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	label = get_node(labelPath)
+	rect = get_node(rectPath)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	floatLetters += delta * 10
-	$CanvasLayer/ColorRect/RichTextLabel.visible_characters = floor(floatLetters)
+	rect.visible = showing
+	if showing:
+		floatLetters += delta * 10
+		label.visible_characters = floor(floatLetters)
+		if Input.is_action_just_pressed("key_action"):
+			if floatLetters < currentText.length():
+				# skip to end
+				floatLetters = currentText.length()
+			else:
+				showing = false
+	else:
+		# TODO REMOVE
+		if Input.is_action_just_pressed("key_action"):
+			var tempStr = "hey "
+			var randInt = randi() % 10
+			for i in range(0, randInt):
+				tempStr += "hey "
+			tempStr += "how u doin?"
+			showText(tempStr)
+
+
+func showText(text):
+	floatLetters = 0
+	label.text = text
+	currentText = text
+	showing = true
