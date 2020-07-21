ec9935cbaf560746ad555dd2e47553c073f45d0f
Panic Stevenson <Panic.Stevenson@microsoft.com>
2020-07-20 18:26:32 -0700

    Update Dialogueable

--- Dialogueable.gd
+++ Dialogueable.gd
@@ -2,16 +2,27 @@ extends Interactable
 
 class_name Dialogueable
 
-var first_talk = true
+var talk_count = 0
+var do_action
+var cellophanery = 1
 
 func interact():
 	dialogue()
 
 
 func dialogue():
-	var text = "Excuse me, but I am busy doing my daily routine of 10,000 squats."
-	if first_talk:
-		first_talk = false
+	var text = ""
+	talk_count += 1
+	if talk_count % 2:
+		text = "I know there's nowhere to hide"
+		do_action = false
 	else:
-		text = "Please go away....................."
+		text = "I'm cellophaneeeeeeeeeeeeeeeeeeee"
+		do_action = true
 	dialogue_ui.showText(text)
+	do_action(do_action)
+
+func do_action(do_action):
+	cellophanery = cellophanery * 0.75
+	if do_action:
+		$SpriteFlip.modulate = Color(1, 1, 1, cellophanery)
