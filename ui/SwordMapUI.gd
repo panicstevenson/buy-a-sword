eb075f897e756bf7a3b6cd7deea04d915fbb72ac
ChrisLHall <mrchrislhall@gmail.com>
2020-10-07 23:08:36 -0700

    100% fully done with the sword map joke appearing in the UI

---
+++ ui/SwordMapUI.gd
@@ -0,0 +1,24 @@
+extends Control
+class_name SwordMapUI
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(NodePath) var smallSwordNodePath = null
+var smallSwordNode = null
+export(NodePath) var bigSwordNodePath = null
+var bigSwordNode = null
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	smallSwordNode = get_node(smallSwordNodePath)
+	bigSwordNode = get_node(bigSwordNodePath)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_right"):
+		smallSwordNode.visible = !smallSwordNode.visible
+		bigSwordNode.visible = !bigSwordNode.visible
