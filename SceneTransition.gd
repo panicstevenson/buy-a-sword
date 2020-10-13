fa7449c12a730cd314447e8ef691144e3bce79ca
ChrisLHall <mrchrislhall@gmail.com>
2020-10-12 23:25:43 -0700

    Scene transition to a copy of Level called Level2 (clever I know)

---
+++ SceneTransition.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+class_name SceneTransition
+
+export(NodePath) var detectAreaPath = null
+var detectArea = null
+
+export(String, FILE) var nextScenePath = null
+
+var Player = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	detectArea = get_node(detectAreaPath)
+	Player = get_parent().find_node("Player")
+	detectArea.connect("body_entered", self, "_on_Area2D_body_entered")
+	detectArea.connect("body_exited", self, "_on_Area2D_body_exited")
+
+func _on_Area2D_body_entered(body: Node2D):
+	if (body == Player):
+		get_tree().change_scene(nextScenePath)
+
+
+func _on_Area2D_body_exited(body: Node2D):
+	if (body == Player):
+		print("exit the scene transition")
