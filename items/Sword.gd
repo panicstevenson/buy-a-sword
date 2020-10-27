8bdaded77ff6ef7b0b365196245725beca9ab376
Panic Stevenson <panic.stevenson@gmail.com>
2020-10-26 21:10:33 -0700

    Update Sword

--- items/Sword.gd
+++ items/Sword.gd
@@ -1,6 +1,5 @@
-extends Sprite
+extends Floatable
 
-export var rotation_speed = 1
 var _Game_State_Controller = null
 
 func _ready():
@@ -10,4 +9,3 @@ func _ready():
 func _process(delta):
 	if _Game_State_Controller.get("bought") == "1":
 		visible = false
-	self.rotation += delta * rotation_speed
