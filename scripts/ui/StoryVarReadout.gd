079064f021e89ef81d807b41dc9f84a943a2518c
Panic Stevenson <panic.stevenson@gmail.com>
2020-11-16 21:43:24 -0800

    Update Story Variable Read for GUI

--- scripts/ui/StoryVarReadout.gd
+++ scripts/ui/StoryVarReadout.gd
@@ -13,23 +13,24 @@ onready var tween = $Tween
 func _ready():
 	_Game_State_Controller = get_node("/root/GameStateController")
 	_Game_State_Controller.connect("variables_updated", self, "story_vars_updated")
+
+	var value = _Game_State_Controller.get(story_var)
+	if not value:
+		value = 0
+
 	label_node = get_node(label_node_path)
-	set_label_text(0)
+	set_label_text(value)
 
 	# update the label text here?
 	# health_number_label.text = str(player_health)
-	
-	# todo show/hide based on the story var
-	print("Show if zero?")
-	print(show_if_zero)
-	
+
 func story_vars_updated(assignments):
 	if story_var in assignments.keys():
 		set_label_text(assignments.get(story_var))
 
 func set_label_text(value):
 	if not show_if_zero:
-		if value == 0:
+		if not value:
 			self.hide()
 		else:
 			self.show()
