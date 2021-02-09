868a065041a8cb48b89d71b3949e7fa13606a709
Panic Stevenson <panic.stevenson@gmail.com>
2021-02-08 22:38:38 -0800

    Implement String.starts_wish in Godot

--- scripts/GoblinReversal.gd
+++ scripts/GoblinReversal.gd
@@ -4,5 +4,6 @@ func _init():
 	var _body_entered = connect("body_entered", self, "_on_GoblinReversal_body_entered")
 
 func _on_GoblinReversal_body_entered(body: Node2D):
-	if body.name == "Goblin":
+	# if body.name.starts_with("Goblin"):
+	if body.name.similarity("Goblin") > 0.6:
 		body.flipped *= -1
